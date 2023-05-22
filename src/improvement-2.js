6760c6dd4dfcea4d34f19e3d03283c860a19721f
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-05-22 19:00:37 +0200

    feat: improve the code quality

--- src/improvement-2.js
+++ src/improvement-2.js
@@ -5,5 +5,6 @@
  */
 
 // FUNCTION 2
-export default processItems = (items) =>
-  items.filter((item) => item.type === "Food");
+export default function processItems(items) {
+  return items.filter((item) => item.type === "Food");
+}
